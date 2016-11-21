157538cd739ec9e1cdea77a985a7429f498cd404
Rafael Almeida <rafaelcpalmeida@gmail.com>
2016-11-21 15:43:07 +0000

    Implemented async chrome storage get with promises

--- js/content.js
+++ js/content.js
@@ -1,29 +1,34 @@
-function applyReplacementRule(node) {
-    var hwReplacements = ["teste", "velocidade", "não habilitados", "de"];
-    var hwBannedTags = ["STYLE", "SCRIPT", "NOSCRIPT", "TEXTAREA"];
+var hwReplacements;
+var hwBannedTags = ["STYLE", "SCRIPT", "NOSCRIPT", "TEXTAREA"];
 
+function applyReplacementRule(node) {
 	// Ignore any node whose tag is banned
 	if( !node || $.inArray( node.tagName, hwBannedTags ) !== -1 ) { return; }
 
-	try 
-	{
+	try {
 		$(node).contents().each(function(i, v) {
 			// Ignore any child node that has been replaced already or doesn't contain text
 			if( v.isReplaced || v.nodeType !== Node.TEXT_NODE ) { return; }
 
 			// Apply each replacement in order
-			hwReplacements.forEach( function(replacement) {
-				//if( !replacement.active ) return;
-                var matchedText = v.textContent.match(new RegExp(replacement, "i"));
+            hwReplacements.then(function(replacements) {
+                replacements.palavras.forEach( function(replacement) {
+                    //if( !replacement.active ) return;
+                    var matchedText = v.textContent.match(new RegExp(replacement, "i"));
 
-                if (matchedText) {
-                    // Use `` instead of '' or "" if you want to use ${variable} inside a string
-                    // For more information visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-                    var replacedText = node.innerHTML.replace(new RegExp(`(${replacement})`, "i"), "<span class='toHighlight' style=\"background-color: yellow\">$1</span>");
+                    if (matchedText) {
+                        // Use `` instead of '' or "" if you want to use ${variable} inside a string
+                        // For more information visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+                        var replacedText = node.innerHTML.replace(new RegExp(`(${replacement})`, "i"), "<span class='toHighlight' style=\"background-color: yellow\">$1</span>");
 
-                    node.innerHTML = replacedText;
-                }
-			});
+                        node.innerHTML = replacedText;
+                    }
+                })
+            }).catch(
+            // Log the rejection reason
+            function(reason) {
+                console.log('Handle rejected promise ('+reason+') here.');
+            });;
 
 			v.isReplaced = true;
 		});
@@ -33,5 +38,17 @@ function applyReplacementRule(node) {
 		{ throw err; }
 	}
 }
+
+chrome.storage.local.clear();
+
+var words = ["teste", "velocidade"];
+
+chrome.storage.local.set({ "words": words }, function () { });
+
+hwReplacements = new Promise(function(resolve, reject) {
+    chrome.storage.local.get("words", function (items) {
+        resolve(items);
+    });
+});
 
 $("body *").map(function(i, v) { applyReplacementRule(v); } );
