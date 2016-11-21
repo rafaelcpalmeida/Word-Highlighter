b7c5e8c41348a878639babf5b2c8a5e09d46948f
Rafael Almeida <rafaelcpalmeida@gmail.com>
2016-11-21 12:53:13 +0000

    Minor updates

--- js/content.js
+++ js/content.js
@@ -1,25 +1,27 @@
 $('body *').map(function(i, v) { applyReplacements(v); } );
 
 function applyReplacements(node) {
-    var g_replacements = ['teste'];
-    var g_bannedtags = ['STYLE', 'SCRIPT', 'NOSCRIPT', 'TEXTAREA'];
+    var gReplacements = ["teste", "velocidade", "não habilitados"];
+    var gBannedTags = ["STYLE", "SCRIPT", "NOSCRIPT", "TEXTAREA"];
 
 	// Ignore any node whose tag is banned
-	if( !node || $.inArray( node.tagName, g_bannedtags ) !== -1 ) return;
+	if( !node || $.inArray( node.tagName, gBannedTags ) !== -1 ) return;
 
 	try 
 	{
 		$(node).contents().each(function(i, v) {
 			// Ignore any child node that has been replaced already or doesn't contain text
-			if( v.isReplaced || v.nodeType != Node.TEXT_NODE ) return;
+			if( v.isReplaced || v.nodeType !== Node.TEXT_NODE ) return;
 
 			// Apply each replacement in order
-			g_replacements.forEach( function(replacement) {
+			gReplacements.forEach( function(replacement) {
 				//if( !replacement.active ) return;
                 var matchedText = v.textContent.match(new RegExp(replacement, "i"));
 
                 if (matchedText) {
-                    var replacedText = node.innerHTML.replace(/(teste)/i, "<span class='toHighlight' style=\"background-color: yellow\">$1</span>");
+                    // Use `` instead of '' or "" if you want to use ${variable} inside a string
+                    // For more information visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+                    var replacedText = node.innerHTML.replace(new RegExp(`(${replacement})`, "i"), "<span class='toHighlight' style=\"background-color: yellow\">$1</span>");
 
                     node.innerHTML = replacedText;
                 }
@@ -29,7 +31,7 @@ function applyReplacements(node) {
 		});
 	} catch( err ) {
 		// Basically this means that an iframe had a cross-domain source, and WR can't do much about it.
-		if( err.name == 'SecurityError' ); 
+		if( err.name === "SecurityError" ); 
 		else throw err;
 	}
 }
